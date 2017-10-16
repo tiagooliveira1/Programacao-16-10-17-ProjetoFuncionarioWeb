20139b3ff28f73992f501425c645e6567b2217b4
Tiago Oliveira <tiago@econtabil.com>
2017-10-16 21:34:49 -0200

    Commit

---
+++ src/opet/funcionario/view/FuncionarioBean.java
@@ -0,0 +1,16 @@
+package opet.funcionario.view;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("funcionarioBean")
+@SessionScoped
+public class FuncionarioBean implements Serializable {
+
+	public FuncionarioBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
