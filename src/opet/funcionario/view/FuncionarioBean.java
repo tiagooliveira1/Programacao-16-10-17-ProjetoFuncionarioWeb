b0159e605ff5d8b35b17fd3581530e21d73413a8
Tiago Oliveira <tiago@econtabil.com>
2017-10-16 22:22:03 -0200

    Commit

--- src/opet/funcionario/view/FuncionarioBean.java
+++ src/opet/funcionario/view/FuncionarioBean.java
@@ -5,6 +5,9 @@ import java.io.Serializable;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import opet.funcionarios.vo.Diretor;
+import opet.funcionarios.vo.Funcionario;
+
 @Named("funcionarioBean")
 @SessionScoped
 public class FuncionarioBean implements Serializable {
@@ -14,14 +17,36 @@ public class FuncionarioBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1979204258589489458L;
 
+	private Funcionario funcionario;
+	
+	private String tipoFuncionario;
 	public FuncionarioBean() {
 		// TODO Auto-generated constructor stub
+		funcionario = new Diretor("", 0.0);
+		tipoFuncionario = "";
+		
 	}
 	
 	public String incluir() {
 		//TODO:: IMPLEMENTAR A CHAMADA DO CONTROLLER
 		//retorna nessa string o arquivo de retorno
 		return "/funcionario/resultadoCadastroFuncionario";
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public String getTipoFuncionario() {
+		return tipoFuncionario;
+	}
+
+	public void setTipoFuncionario(String tipoFuncionario) {
+		this.tipoFuncionario = tipoFuncionario;
 	}
 
 }
